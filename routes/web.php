29280f3a21b26839cd0305aaab727d196e82721e
= <=>
2022-02-21 13:52:04 +0700

    commit pertama

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/ ', function () {
+    echo "Selamat Datang ";
+});
+
+Route::get('/about ', function () {
+    echo "2041720164 Achmad Ainun Naim";
+});
+
+Route::get('/articles/ {id} ', function () {
+    echo " “Halaman Artikel 
+    dengan ID =  2041720164";
+});
